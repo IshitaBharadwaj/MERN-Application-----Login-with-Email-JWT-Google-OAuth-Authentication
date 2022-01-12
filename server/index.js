47c2758e35f858216c0062a7d2d23ddd04306a96
Ishita Bharadwaj <ishitabharadwaj04@gmail.com>
2022-01-12 22:48:08 +0530

    Update index.js
    updated index.js 2

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-const CONNECTION_URL = <!--- Give you MongoDB URL -->;
+const CONNECTION_URL = //Give you MongoDB URL;
 const PORT = process.env.PORT|| 5000;
 
 //const PORT = 3000
