669f1c3c68008cbf01aa79aaf7ec641fdc5b73a1
Ishita Bharadwaj <ishitabharadwaj04@gmail.com>
2022-01-12 22:46:22 +0530

    Update index.js
    Updated index.js in server

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://ishitabharadwaj:iamishita123@cluster0.6bbcj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = <!--- Give you MongoDB URL -->;
 const PORT = process.env.PORT|| 5000;
 
 //const PORT = 3000
